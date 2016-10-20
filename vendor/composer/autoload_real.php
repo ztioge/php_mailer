b10eddc13084204d32814c20370ffb8bf100ebed
ztioge <egoitzu@telefonica.net>
2016-10-20 11:54:50 +0000

    terminado

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit2d0c4bbaa5c6255fcbf22f9263bc04d3
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit2d0c4bbaa5c6255fcbf22f9263bc04d3::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire2d0c4bbaa5c6255fcbf22f9263bc04d3($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire2d0c4bbaa5c6255fcbf22f9263bc04d3($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
